16fa7e2c220de8619be221c86d21cb3c1b90bfb2
aziekayrussell <aziekayrussell@10.17.167.179>
2013-12-09 18:06:48 -0700

    Modifying Menu.java and created TwentyOneError.java to help with error messages

--- src/cit260/main/MainMenuControl.java
+++ src/cit260/main/MainMenuControl.java
@@ -1,46 +1,96 @@
-package cit260.main;
-
-
-import java.io.Serializable;
-
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package ticTacToe;
+
+import java.util.Scanner;
 
 /**
  *
- * @author aziekayrussell
+ * @author Kyle Russell
  */
-public class MainMenuControl implements Serializable {
+
+
+
+
+public class MainMenuView extends Menu {
     
-    public int menu = 0;
+    private static final String[][] menuItems = {
+        {"1", "One player game"},
+        {"2", "Two player game"},
+        {"H", "Help"},
+        {"X", "Exit Tic-Tac-Toe"}
+    }; 
+  
+    MainMenuControl mainCommands = new MainMenuControl();
     
-    static String error = "Error: Invalid input. Please select one of\n"
-                + "the options listed.\n";
-    static String goback = "To return to the help menu, type 'menu'. To quit this\n"
-                + "program, type 'quit'\n";
-    
-    public void descriptionMain(){
-        
-        System.out.println("Welcome to the game 21! Please select one of the\n"
-                + "options below by entering the number next to your selection.");
-        
-        System.out.println("1. New Game"
-                + "2. Help Menu"
-                + "3. Quit Game"
-                + "");
+    public MainMenuView() {
+        super(MainMenuView.menuItems);
     }
     
-    public void error(){
+    @Override
+    public String executeCommands(Object object) {       
         
-        System.out.println(this.error);
+        String gameStatus = Game.PLAYING;
+        do {
+            this.display();
+
+            // get commaned entered
+            String command = this.getCommand();
+            switch (command) {
+                case "1":
+                    this.startGame(1);
+                    break;
+                case "2":
+                    this.startGame(2);
+                    break;
+                case "H":
+                    HelpMenuView helpMenu = TicTacToe.getHelpMenu();
+                    helpMenu.executeCommands(null);
+                    break;
+                case "X":
+                    return Game.EXIT;
+            }
+        } while (!gameStatus.equals(Game.EXIT));
+
+        return Game.EXIT;
     }
-    
-    public void goback(){
+
+    private void startGame(long noPlayers) {
+                
+        if (noPlayers != 1  &&  noPlayers != 2) {
+            new TicTacToeError().displayError("startGame - invalid number of players specified.");
+            return;
+        }
         
-        System.out.println("this.goback");
+        Game game;
+        if (noPlayers == 1) {
+            game = this.mainCommands.create(Game.ONE_PLAYER);
+        }
+        else {
+            game = this.mainCommands.create(Game.TWO_PLAYER);
+        }
+        
+        SelectPlayersView sekectPlayersView = new SelectPlayersView(game);
+        String status = (String) sekectPlayersView.selectPlayers(TicTacToe.getNameList());
+        if (status.equals(Game.QUIT)) {
+            return;
+        }
+
+        GameMenuView gameMenu = new GameMenuView(game);
+        gameMenu.executeCommands(game);
+    }
+
+    private String quitGame() {
+        System.out.println("\n\tAre you sure you want to quit? (Y or N)");
+        Scanner inFile = new Scanner(System.in);
+        String answer = inFile.next().trim().toUpperCase();
+        if (answer.equals("Y")) {
+            return Game.EXIT;
+        }
+
+        return Game.PLAYING;
     }
     
 }
